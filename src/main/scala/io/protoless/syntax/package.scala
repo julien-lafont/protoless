a993ad2e7f6a3b31731cd06a531984370ec2abb4
Julien Lafont <julien.lafont@tabmo.io>
2017-08-11 09:28:08 +0200

    Add syntax file for implicit encoding

---
+++ src/main/scala/io/protoless/syntax/package.scala
@@ -0,0 +1,15 @@
+package io.protoless
+
+import java.io.ByteArrayOutputStream
+
+import io.protoless.core.Encoder
+
+package object syntax {
+
+  implicit final class EncoderOps[A](val wrappedEncodeable: A) extends AnyVal {
+    final def asProtobufBytes(implicit encoder: Encoder[A]): Array[Byte] = encoder.encodeAsBytes(wrappedEncodeable)
+
+    final def asProtobufStream(implicit encoder: Encoder[A]): ByteArrayOutputStream = encoder.encodeAsStream(wrappedEncodeable)
+  }
+
+}
